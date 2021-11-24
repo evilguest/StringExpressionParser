a8963274d0e4d86bb098aa883d8a194039d452b5
Anton Zlygostev <zlygostev@hotmail.com>
2021-11-24 22:48:22 +0700

    Add project files.

---
+++ StringExpressionParser/KeyedDictionary.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringExpressionParser
+{
+    public class KeyedDictionary<K, T> : IReadOnlyDictionary<K, T>
+        where K: notnull
+    {
+        public KeyedDictionary(Func<T, K> keyExtractor) => _keyExtractor = keyExtractor ?? throw new ArgumentNullException(nameof(keyExtractor));
+        private readonly Func<T, K> _keyExtractor;
+
+
+        #region IReadOnlyDictionary<K, T>
+        private readonly Dictionary<K, T> _dictionary = new();
+
+        public T this[K key] => _dictionary[key];
+
+        public IEnumerable<K> Keys => _dictionary.Keys;
+
+        public IEnumerable<T> Values => _dictionary.Values;
+
+        public int Count => ((IReadOnlyCollection<KeyValuePair<K, T>>)_dictionary).Count;
+
+        public bool ContainsKey(K key) => _dictionary.ContainsKey(key);
+
+        public IEnumerator<KeyValuePair<K, T>> GetEnumerator() => _dictionary.GetEnumerator();
+
+        public bool TryGetValue(K key, [MaybeNullWhen(false)] out T value) => _dictionary.TryGetValue(key, out value);
+
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)_dictionary).GetEnumerator();
+        #endregion
+        public void Add(T value) => _dictionary.Add(_keyExtractor(value), value);
+    }
+}
---
+++ StringExpressionParser/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Expressions;
+namespace StringExpressionParser;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        var x = Expression.Parameter(typeof(int), "x");
+        var y = Expression.Parameter(typeof(int), "y");
+        var z = Expression.Parameter(typeof(int), "z");
+
+        var a = new Arithmetics();
+        a.Parameters.Add(x);
+        a.Parameters.Add(y);
+        a.Parameters.Add(z);
+
+        var e = a.Parse("2*x + y + z");
+        var l = Expression.Lambda<Func<int, int, int, int>>(e, x, y, z);
+        var c = l.Compile();
+        Console.WriteLine(c(1, 2, 3));
+         
+    }
+}
