9902519c25cf8899d4220797c4aad875ee65bf78
Anton Zlygostev <zlygostev@hotmail.com>
2021-11-25 08:40:18 +0700

    Converted to double and fixed the power function

--- StringExpressionParser/Program.cs
+++ StringExpressionParser/Program.cs
@@ -13,12 +13,11 @@ public class Program
         var a = new Arithmetics();
         a.Parameters.Add(x, y, z);
 
-        var e = a.Parse("2*x + y + z");
-        var l = Expression.Lambda<Func<int, int, int, int>>(e, x, y, z);
+        var e = a.Parse("2*x + y^3 + z");
+        var l = Expression.Lambda<Func<double, double, double, double>>(e, x, y, z);
         var c = l.Compile();
         Console.WriteLine(c(1, 2, 3));
-
     }
 
-    private static ParameterExpression NewParam(string name) => Expression.Parameter(typeof(int), name);
+    private static ParameterExpression NewParam(string name) => Expression.Parameter(typeof(double), name);
 }
